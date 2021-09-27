28c9a67f625c1eeb678286c863f19415a6c09324
engr425imran <fana9994@yahoo.com>
2021-09-27 23:31:53 +0500

    five

---
+++ database/migrations/2021_09_27_182743_create_deliveries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDeliveriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deliveries', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('name');
+            $table->string('sender_address');
+            $table->string('delivery_address');
+            $table->string('email');
+            $table->string('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deliveries');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\DeliveryController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +17,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('/deliveries/edit/{id}', [DeliveryController::class, 'edit']);
+Route::get('/deliveries/show/{id}', [DeliveryController::class, 'show']);
+
+Route::get('/getdata', [DeliveryController::class, 'index']);
+Route::post('/savedata', [DeliveryController::class, 'store']);
+
+// ---------------------------------------------------
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/sanctum/token', [ApiController::class, 'create']);
+Route::post('/sanctum/login', [ApiController::class, 'login']);
