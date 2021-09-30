8d441af2f4c3c8abd3dcf2ef3fda9e11f52b185a
engr425imran <fana9994@yahoo.com>
2021-09-30 21:23:15 +0500

    validation3.3

--- routes/api.php
+++ routes/api.php
@@ -34,7 +34,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::middleware('auth:sanctum')->post('/sanctum/logout',[ApiController::class, 'logout']);
-});
+
 Route::post('/sanctum/token', [ApiController::class, 'create']);
 Route::post('/sanctum/login', [ApiController::class, 'login']);
 
